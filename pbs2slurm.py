5cae6771e8ff70ddf0ef15f3a8c55a8844d0527a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-04-23 20:03:36 +0900

    引数を解析
    -e path
    -N name
    -o path
    -v variable_list

--- pbs2slurm.py
+++ pbs2slurm.py
@@ -13,14 +13,35 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
+        '-e', dest='stderr_file',
+        type=str, required=False, default=None,
+        metavar='path',
+    )
+    parser.add_argument(
+        '-N', dest='job_name',
+        type=str, required=False, default=None,
+        metavar='name',
+    )
+    parser.add_argument(
+        '-o', dest='output',
+        type=str, required=False, default=None,
+        metavar='path',
+    )
+
+    parser.add_argument(
         '-P', dest='project',
-        type=str, required=False, default='',
+        type=str, required=False, default=None,
         metavar='project',
     )
     parser.add_argument(
         '-q', dest='queue',
-        type=str, required=False, default='',
+        type=str, required=False, default=None,
         metavar='distination',
+    )
+    parser.add_argument(
+        '-v', dest='variable_list',
+        type=str, required=False, default=None,
+        metavar='variable_list',
     )
 
     return  parser.parse_args()
